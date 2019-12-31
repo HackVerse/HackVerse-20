8c3163d6c4b2d2579efeb6725944d4ba919e2d22
mehnazyunus <mehnaz138@gmail.com>
2019-12-31 09:45:59 +0530

    Fix timer

--- js/default-assets/active.js
+++ js/default-assets/active.js
@@ -225,8 +225,8 @@
     // :: 13.0 Countdown Active Code
     // ****************************
     if ($.fn.countdown) {
-        $("#clock").countdown("2020/01/26", function (event) {
-            $(this).html(event.strftime("<div>%m <span>Months</span></div> <div>%d <span>Days</span></div> <div>%H <span>Hours</span></div> <div>%M <span>Minutes</span></div> <div>%S <span>Seconds</span></div>"));
+        $("#clock").countdown("2020/01/26 08:00:00", function (event) {
+            $(this).html(event.strftime("<div>%D <span>Days</span></div> <div>%H <span>Hours</span></div> <div>%M <span>Minutes</span></div> <div>%S <span>Seconds</span></div>"));
         });
     }
 
