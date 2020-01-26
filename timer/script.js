b93bc59380f3264633e052fd27690262f2a916fd
K Rahul Reddy <31247036+krahulreddy@users.noreply.github.com>
2020-01-26 09:45:36 +0530

    Updated end time

--- timer/script.js
+++ timer/script.js
@@ -3,7 +3,7 @@ const second = 1000,
     hour = minute * 60,
     day = hour * 24;
 
-let countDown_end = new Date('Jan 26, 2020 16:30:00').getTime()
+let countDown_end = new Date('Jan 26, 2020 17:00:00').getTime()
 let countDown_start = new Date('Jan 25, 2020 17:00:00').getTime()
 let input_flag = false
 let input = ""
